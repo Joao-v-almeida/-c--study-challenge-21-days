4989476a6cf484d0f1ed00011288b318e72bd494
Joao-v-almeida <jaovittor.almeida@gmail.com>
2022-12-07 21:42:11 -0300

    applying the msTest

--- ProgramTest/Models/ClientTest.cs
+++ ProgramTest/Models/ClientTest.cs
@@ -1,6 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using program.Models;
+
 namespace ProgramTest.Models;
 
+[TestClass]
 public class ClientTest
 {
+    [TestMethod]
+    public void PropertiesTest()
+    {
+        var client = new Client();
+        client.Id = "1";
+        client.Nome = "João Almeida";
+        client.Telefone = "43 99644-7033";
+        client.Email = "joao@teste.com";
 
+        Assert.AreEqual("1", client.Id);
+        Assert.AreEqual("João Almeida", client.Nome);
+        Assert.AreEqual("43 99644-7033", client.Telefone);
+        Assert.AreEqual("joao@teste.com", client.Email);
+    }
 }
